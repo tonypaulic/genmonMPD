7c619db9bc1b4116ec8abb7344f48e200906a101
tonypaulic <tony.paulic@gmail.com>
2025-08-23 21:39:03 -0400

    Deal with &s

--- music.sh
+++ music.sh
@@ -12,7 +12,12 @@ S3="$(echo -e '\U2707')"
 
 # extract the metadata
 IFS=';' read -r FILE TITLE ARTIST <<< $(mpc -f "%file%;%title%;%artist%" | head -1)
-STATUS="<big>$S1</big>  $(mpc status | sed -e "s/\[/\n$S2   [/" | sed -e "s/volume/\n$S3   volume/")"
+	# deal with the ampersand so pango can handle it
+	TITLE="$(echo $TITLE | sed -e "s/&/&amp;/g")"
+	ARTIST="$(echo $ARTIST | sed -e "s/&/&amp;/g")"
+STATUS="<big>$S1</big>  $(mpc status | sed -e "s/&/&amp;/g" | \
+													sed -e "s/\[/\n$S2   [/" | \
+													sed -e "s/volume/\n$S3   volume/")"
 
 # if no title, show filename
 [[ "$TITLE" == "" ]] && TITLE="$FILE" && ARTIST="(no metadata)"
@@ -22,7 +27,7 @@ if [ "$FILE" != "$(cat /tmp/.music.txt)" ]; then
 
 	# extract and resize the cover art
 	ffmpeg -y -i "$MUSIC/$FILE" -an -c:v copy /tmp/.music.jpg > /dev/null 2>&1
-	[[ $? -eq 0 ]] || cp /home/toz/vinyl.jpg /tmp/.music.jpg
+		[[ $? -eq 0 ]] || cp /home/toz/vinyl.jpg /tmp/.music.jpg
 	magick /tmp/.music.jpg -resize 32x32 /tmp/.music.jpg
 	
 	# save the filename for next iteration comparison
